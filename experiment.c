dbef89d6e72c40fc71f89f98e77b04c061735d97
Yadhunandana Rajathadripura Kumaraiah <yadhunandana.kumaraiah@rohde-schwarz.com>
2023-07-05 11:29:35 +0200

    Added reverse linked list working example

--- experiment.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-main()
-{
-
-	printf("yadhu here\n");
-}
--- reverseLinkedList/reverseLinkedList.cpp
+++ reverseLinkedList/reverseLinkedList.cpp
@@ -1,72 +1,100 @@
-//============================================================================
-// Name        : reverseLinkedList.cpp
-// Author      : Yadu
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
-
 #include <iostream>
+#include <memory>
+#include <cstdint>
 
 #define NODES_COUNT 100
+class Node 
+{
+public:
+    int value;
+    Node* next;
 
-using namespace std;
-
-struct node {
-	int value;
-	node *next;
+    ~Node()
+    {
+        if(next != nullptr) {
+            free(next);
+        }
+    }
 };
 
-void print_list(struct node *head)
+void print_list(Node* head)
 {
-	struct node *curr = head;
-	while(curr != NULL)
-	{
-		cout<<curr->value<<endl;
-		curr = curr->next;
-	}
+    if(head == nullptr) {
+        std::cout<<"empty list"<<std::endl;
+        return;
+    }
+
+    Node* curr = head;
+    while(curr != NULL)
+    {
+        std::cout<<curr->value<<", address: "<<curr << ", next: "<<curr->next <<std::endl;
+        curr = curr->next;
+    }
+    std::cout<<std::endl;
+}
+
+Node* createList(std::uint32_t listSize)
+{
+    if(listSize == 0) {
+        std::cout <<"List can be created only with nonzero size"<<std::endl;
+        return nullptr;
+    }
+
+    Node* head = new Node();
+    head->value = 0;
+    head->next = nullptr;
+    Node* curr = head;
+    for(std::uint32_t counter = 1; counter < listSize; ++counter)
+    {
+        curr->next = new Node();
+        curr = curr->next;
+        curr->value = counter;
+    }
+    return head; // returning the head of the list
+}
+
+/**
+ * @brief Reverses the singly linked list and returns the new head
+ * 
+ * @param head 
+ */
+Node* reverseList(Node* head)
+{
+    if(head == nullptr || head->next == nullptr) {
+        std::cout <<"List is empty"<<std::endl;
+        return head;
+    }
+
+    Node* prev = head;
+    Node* curr  = head->next;
+    head->next = nullptr;
+    Node* next = curr->next;
+
+    while(curr != nullptr)
+    {
+        // std::cout<<"Exchanginging the nodes"<<std::endl;
+        Node* currNext = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = currNext;
+    }
+    return prev;
 }
 
 int main() {
-	cout << "!!!Hello World to reverse linked list!!!" << endl; // print
+    std::cout << "!!!Hello World to reverse linked list!!!" << std::endl; // print
 
-	struct node *prev = NULL,*head = NULL,*curr = NULL,*next = NULL;
+    Node* head = createList(0);
 
-	//Special handling is needed for tail, hence doing outside the loop.
-	curr = (struct node*) malloc(sizeof(struct node));
-	curr->value = NODES_COUNT - 1;
-	curr->next = NULL;
-	prev = curr;
+    print_list(head);
 
-	for(int i = NODES_COUNT - 2; i >= 0 ; i--)
-	{
-		curr = (struct node*) malloc(sizeof(struct node));
-		curr->value = i;
-		curr->next = prev;
-		prev = curr;
-	}
-	head = curr; //last created node is the head of the list.
+    head = createList(10);
 
-	print_list(head);
+    print_list(head);
 
-	//Special handling is needed for head, hence doing outside the loop.
-	curr = head;
-	next = curr->next;
-	curr->next = NULL;
-	curr = next;
-	prev = head;
+    head = reverseList(head);
 
-	while(next != NULL)
-	{
-		next = curr->next;
-		curr->next = prev;
-		prev = curr;
-		curr = next;
-	}
+    print_list(head);
 
-	head = curr = prev;
-	cout<<"Reversed List"<<endl;
-	print_list(head);
-
-	return 0;
+    return 0;
 }
